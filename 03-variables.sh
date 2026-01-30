a72bc0636255ab2450226717924faf5137f2dc88
sumanthmerugu <suma9th9@gmail.com>
2026-01-30 12:27:38 -0500

    created Person1

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1="Sumanth"
+PERSON2="Viggi"
+
+echo "$PERSON1: Hello! What's your name?"
+echo "$PERSON2:: Hi $PERSON1! My name is $PERSON2. What's yours?"
+echo "$PERSON1:: I'm $PERSON1. Nice to meet you, $PERSON2!"
+echo "$PERSON2:: Nice to meet you too, $PERSON1!"
