e96e8269c3bc437f178653fbd429e4bf5dad8d0d
sumanthmerugu <suma9th9@gmail.com>
2026-01-30 11:36:19 -0500

    I wrote just conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Sumanth:: Hello! What's your name?"
+echo "Viggi:: Hi Sumanth! My name is Viggi. What's yours?"
+echo "Sumanth:: I'm Sumanth. Nice to meet you, Viggi!"
+echo "Viggi:: Nice to meet you too, Sumanth!"
