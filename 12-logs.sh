f1a008be28701e83ef57862cc37c41fbf1c15f91
sumanthmerugu <suma9th9@gmail.com>
2026-02-22 00:15:53 -0500

    created data types

--- 12-logs.sh
+++ 12-logs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER= "/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access"
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 
@@ -13,10 +13,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo "$2 ... failed"
+        echo "$2 ... failed" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 ... success"
+        echo "$2 ... success" | tee -a $LOGS_FILE
     fi
 }
 
