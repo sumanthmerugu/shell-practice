70c504a24534fa8720c4acabee36dda573636b6d
sumanthmerugu <suma9th9@gmail.com>
2026-02-21 23:39:42 -0500

    created data types

---
+++ 12-logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FLODER= "/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+mkdir -p $LOGS_FLODER
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... failed"
+        exit 1
+    else
+        echo "$2 ... success"
+    fi
+}
+
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "Installing MySQL"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing NodeJS"
