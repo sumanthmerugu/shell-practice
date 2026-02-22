d82fd24b4ed9640188a2bff1ba2873598becbd9e
sumanthmerugu <suma9th9@gmail.com>
2026-02-22 01:40:35 -0500

    created data types

--- 19-trap.sh
+++ 19-trap.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+trap 'echo "There is an error in $LINENO, Command; $BASH_COMMAND"' ERR
+
 echo "Hello World"
 echo "This is a trap example"
 echoo "This line will cause an error and the script will exit immediately due to set -e"
